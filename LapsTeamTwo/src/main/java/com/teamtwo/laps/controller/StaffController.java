d6ea44d0584537ac472a83fb2d92d8e0384a4558
xinyi <xinyi@172.17.250.221>
2017-06-13 17:15:43 +0800

    Did staff-leave-new

--- LapsTeamTwo/src/main/java/com/teamtwo/laps/controller/StaffController.java
+++ LapsTeamTwo/src/main/java/com/teamtwo/laps/controller/StaffController.java
@@ -1,34 +1,19 @@
 package com.teamtwo.laps.controller;
 
-import static org.hamcrest.CoreMatchers.startsWith;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 
-import java.awt.Dialog.ModalExclusionType;
 import java.text.DateFormat;
-import java.text.DateFormatSymbols;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-import java.util.stream.Collectors;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import org.hibernate.Session;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.xml.ResourceEntityResolver;
-import org.springframework.http.HttpRequest;
-import org.springframework.security.web.context.HttpRequestResponseHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -42,9 +27,6 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.teamtwo.laps.javabeans.LeaveStatus;
 import com.teamtwo.laps.model.Leave;
 import com.teamtwo.laps.model.LeaveType;
-import com.teamtwo.laps.javabeans.DashboardBean;
-import com.teamtwo.laps.javabeans.LeaveStatus;
-import com.teamtwo.laps.model.Leave;
 import com.teamtwo.laps.model.StaffMember;
 import com.teamtwo.laps.service.LeaveService;
 import com.teamtwo.laps.service.LeaveTypeService;
@@ -62,7 +44,6 @@ import com.teamtwo.laps.service.StaffMemberService;
 @RequestMapping(value = "/staff")
 public class StaffController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	private final int DASHBOARD_NUM_TO_SHOW = 3;
 	
 	@Autowired
 	private StaffMemberService smService;
@@ -77,85 +58,17 @@ public class StaffController {
 	 * Renders the staff dashboard.
 	 */
 	@RequestMapping(value = "/dashboard")
-	public ModelAndView home(HttpSession session) {
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
 		
-		UserSession userSession = (UserSession) session.getAttribute("USERSESSION");
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
-		if (userSession == null || userSession.getSessionId() == null) {
-			return new ModelAndView("redirect:/home/login");
-		}
+		String formattedDate = dateFormat.format(date);
 		
-		int staffId = userSession.getEmployee().getStaffId();
-		String userid = userSession.getUser().getUserId();
+		model.addAttribute("serverTime", formattedDate );
 		
-		StaffMember staffMember = smService.findStaffById(staffId);
-		ArrayList<Leave> leaves = lService.findAllLeaveOfStaff(staffId);
-		ModelAndView modelAndView = new ModelAndView("staffDashboard");
-		
-		modelAndView = DashboardBean.getDashboard(modelAndView, DASHBOARD_NUM_TO_SHOW, staffMember, leaves);
-
-//		leaves.get(1).getStartDate().getDate()
-		logger.info("Rendering dashboard for user {}.", userid);
-		
-		return modelAndView;
-	}
-	
-	@RequestMapping(value = "/movement", method = RequestMethod.GET)
-	public ModelAndView movement(HttpSession session) {
-		
-		UserSession userSession = (UserSession) session.getAttribute("USERSESSION");
-		
-		if (userSession == null || userSession.getSessionId() == null) {
-			//return new ModelAndView("redirect:/home/login");
-		}
-		
-		//int staffId = userSession.getEmployee().getStaffId();
-		//String userid = userSession.getUser().getUserId();
-		
-		int currentMonth = Calendar.getInstance().get(Calendar.MONTH);
-		int currentYear = Calendar.getInstance().get(Calendar.YEAR);
-		ModelAndView modelAndView = new ModelAndView("staff-movement-register", "monthSelect", Integer.class);
-		
-		modelAndView.addObject("picked", currentMonth);
-		
-		return getMovementMAV(modelAndView, currentMonth, currentYear);
-	}
-	
-	private ModelAndView getMovementMAV(ModelAndView modelAndView, int currentMonth, int currentYear) {
-		List<Leave> allLeave = lService.findAllLeave().stream().filter(leave -> {
-			Calendar start = Calendar.getInstance();
-			start.setTime(leave.getStartDate());
-			Calendar end = Calendar.getInstance();
-			end.setTime(leave.getEndDate());
-			if ((start.get(Calendar.YEAR) == currentYear && start.get(Calendar.MONTH) == currentMonth)
-					|| (end.get(Calendar.YEAR) == currentYear && end.get(Calendar.MONTH) == currentMonth))
-				return true;
-			else
-				return false;
-		}).collect(Collectors.toList());
-		
-		modelAndView.addObject("allLeave", allLeave);
-		modelAndView.addObject("year", currentYear);
-		modelAndView.addObject("monthName", new SimpleDateFormat("MMMM").format(Calendar.getInstance().getTime()));
-		modelAndView.addObject("allMonthNames", Arrays.asList(new DateFormatSymbols().getMonths()).subList(0, 12));
-		
-		return modelAndView;
-	}
-	
-	
-	@RequestMapping(value = "/movement", method = RequestMethod.POST)
-	public ModelAndView movementPost(HttpServletRequest request, HttpServletResponse response) {
-		String picker = request.getParameter("monthPicker");
-		
-		int currentMonth = Integer.parseInt(picker);
-		int currentYear = Calendar.getInstance().get(Calendar.YEAR);
-		ModelAndView modelAndView = new ModelAndView("staff-movement-register", "monthSelect", Integer.class);
-		
-		modelAndView = getMovementMAV(modelAndView, currentMonth, currentYear);
-		
-		modelAndView.addObject("picked", picker);
-		
-		return modelAndView;
+		return "staffDashboard";
 	}
 	
 	@RequestMapping(value = "/view/{staffId}")
@@ -175,39 +88,28 @@ public class StaffController {
 //	}
 	
 	@RequestMapping(value = "/leave/create", method = RequestMethod.GET)
-	public ModelAndView NewLeavePage() {
+	public ModelAndView NewLeavePage(HttpSession session) {
+		
+		UserSession us = (UserSession) session.getAttribute("USERSESSION");
+		
+		if (us == null) {
+			return new ModelAndView("redirect:/home/login");
+		}
+		
+		int loggedInStaffId = us.getUser().getStaffId();
+		
 		ModelAndView mav = new ModelAndView("staff-leave-new");
 		
-		List<LeaveType> leaveTypes = lTypeService.findAllLeaveType();
-		
+		ArrayList<LeaveType> leaveTypes = lTypeService.findAllLeaveType();
+		ArrayList<StaffMember> staffMembers = (ArrayList<StaffMember>) smService.findAllStaff().stream()
+				.filter(staff -> staff.getStaffId() != loggedInStaffId).collect(Collectors.toList());
 		Leave leave = new Leave();
 		mav.addObject("leave", leave);
 		mav.addObject("leaveTypes", leaveTypes);
+		mav.addObject("staffMembers", staffMembers);
 		
 		return mav;
-	}
-	@RequestMapping(value = "/history")
-	public ModelAndView employeeCourseHistory(HttpSession session) {
-		UserSession us = (UserSession) session.getAttribute("USERSESSION");
-		ModelAndView mav = new ModelAndView("login");
-		if (us.getSessionId() != null) {
-			mav = new ModelAndView("/staff-leave-history");
-			mav.addObject("lhistory", lService.findLeaveById(us.getUser().getStaffId()));
-			return mav;
-		}
-		return mav;
-
-	}
-	
-	@RequestMapping(value = "/logout")
-	public String logout(HttpSession session) {
-		session.invalidate();
-		return "redirect:/home/login";
-
-	}
-	
-	
-	
+}
 	
 	
 	
